c5a38cbdb02c475c8f321c2a4ba7e9d4bbaa298f
TomFiz <106872006+TomFiz@users.noreply.github.com>
2023-12-05 14:52:08 +0000

    training via new method

--- Studying unknown bias in bios dataset/BertMultiClf_1_DataPrep.py
+++ Studying unknown bias in bios dataset/BertMultiClf_1_DataPrep.py
@@ -7,11 +7,11 @@ import pandas as pd
 #PART 1 : same as usual on the bios dataset
 #+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
 
+
 #1.1) load data
 data = pickle.load(open('BIOS.pkl','rb'))
-
-
 print(len(data))
+
 
 #1.2) show data
 
@@ -36,8 +36,8 @@ for i in range(len(data)):
 #https://www.kaggle.com/code/goutham794/distill-bert-fine-tuning-huggingface-and-pytorch/notebook
 #+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
 
-#2.1) init
 
+#2.1) init
 
 import torch
 import torch.nn as nn
@@ -51,6 +51,7 @@ from torch import cuda
 device='cpu'
 
 print('Device is',device)
+
 
 #2.2) pre-processing...
 
@@ -71,8 +72,6 @@ def remove_html(text):
 def remove_username(text):
     url = re.compile(r'@[A-Za-z0-9_]+')
     return url.sub(r'',text)
-
-
 
 def pre_process_text(text):
     text = remove_URL(text)
@@ -137,10 +136,6 @@ def truncation_X(X_raw, n):
 
 #... pre-processing
 
-
-
-
-
 X_raw_sn=[]
 Titles_sn=[]
 Gen_sn=[]
@@ -151,10 +146,8 @@ for i in range(len(X_raw)):
     Titles_sn.append(Titles[i])
     Gen_sn.append(Gen[i])
     
-    
 print(len(X_raw),' -> ',len(X_raw_sn))
      
-
 
 #2.3) create the tokenizer and the functions to get the input and output observations at the torch format
 
@@ -174,20 +167,17 @@ def get_input(input_id,list_input_texts,tokenizer,Max_len):
 
   return [ids,mask]
 
-
-
 def CreateConversions_jobs_jobids(Titles):
 
   job_2_jobid={}
   jobid_2_job={}
 
-  Titles_set=list(set(Titles))
+  Titles_set, _ = np.unique(Titles, return_index=True)
   for i in range(len(Titles_set)):
     job_2_jobid[Titles_set[i]]=i
     jobid_2_job[i]=Titles_set[i]
 
   return [job_2_jobid,jobid_2_job]
-
 
 [job_2_jobid,jobid_2_job]=CreateConversions_jobs_jobids(Titles)
 
@@ -202,57 +192,57 @@ def get_output_id(input_id,lst_outputs,job_2_jobid):
 
 #2.3.bis) transform the tokenized data as classic training X and y
 
+
 #... X
 list_all_ids=[]
 list_all_masks=[]
+list_selected_ids=[]
 n=len(X_raw_sn)
 largest_tokens_nb=0
 
 for i in range(n):
-  print(i,'/',n,'    -    ',largest_tokens_nb)
+  if Titles_sn[i] in ['chiropractor','dentist','nurse','physician','surgeon']:
+    print(i,'/',n,'    -    ',largest_tokens_nb)
 
-  [ids,mask]=get_input(i,X_raw_sn,tokenizer,255)
-  list_all_ids.append(ids)
-  list_all_masks.append(mask)
+    [ids,mask]=get_input(i,X_raw_sn,tokenizer,255)
+    list_selected_ids.append(i)
+    list_all_ids.append(ids)
+    list_all_masks.append(mask)
 
-  if largest_tokens_nb<len(ids):
-    largest_tokens_nb=len(ids)
+    if largest_tokens_nb<len(ids):
+      largest_tokens_nb=len(ids)
 
 p=largest_tokens_nb
 
+m = len(list_selected_ids)
+X=torch.zeros([m,p],dtype=torch.long)
+Masks=torch.zeros([m,p],dtype=torch.long)
 
-X=torch.zeros([n,p],dtype=torch.long)
-Masks=torch.zeros([n,p],dtype=torch.long)
-
-for i in range(n):
-  print(i)
+for i in range(m):
   p_loc=len(list_all_ids[i])
   X[i,:p_loc]=list_all_ids[i]*1
   Masks[i,:p_loc]=list_all_masks[i]*1
 
 #... y
 
-y=torch.zeros([n,len(set(Titles_sn))],dtype=torch.float32)
+y=torch.zeros([m,len(set(Titles_sn))],dtype=torch.float32)
 
-for i in range(n):
+for i in range(m):
     #print(i)
-    y[i,get_output_id(i,Titles_sn,job_2_jobid)]=1
+    y[i,get_output_id(list_selected_ids[i],Titles_sn,job_2_jobid)]=1
 
 #... gender
 
-g=torch.zeros([n],dtype=torch.uint8)
+g=torch.zeros([m],dtype=torch.uint8)
 
-for i in range(n):
-  if Gen_sn[i]=='M':
+for i in range(m):
+  if Gen_sn[list_selected_ids[i]]=='M':
     #print(i)
     g[i]=1
 
 
 data2save ={"X":X,"Masks":Masks,"y":y,'g':g,'bio':X_raw_sn,"job_2_jobid":job_2_jobid,"jobid_2_job":jobid_2_job}
-pickle.dump(data2save, open( "TreatedData_all4.pk", "wb" ) )
-
-
-
+pickle.dump(data2save, open( "TreatedData_medical_all.pk", "wb" ) )
 
 #make sure that the amount of test data in each group is well balanced
 
@@ -284,7 +274,6 @@ for i in range(28):
 np.random.shuffle(TestID)
 np.random.shuffle(TrainID)
 
-
 X_test=X[TestID,:]
 Masks_test=Masks[TestID,:]
 y_test=y[TestID,:]
@@ -301,13 +290,10 @@ bio_train=[]
 for i in range(len(TrainID)):
   bio_train.append(X_raw_sn[TrainID[i]])
 
-
 data2save ={"X_test":X_test,"Masks_test":Masks_test,"y_test":y_test,'g_test':g_test,'bio_test':bio_test , "X_train":X_train,"Masks_train":Masks_train,"y_train":y_train,'g_train':g_train,'bio_train':bio_train , "job_2_jobid":job_2_jobid,"jobid_2_job":jobid_2_job}
-pickle.dump(data2save, open( "TreatedData_all5.pk", "wb" ) )
+pickle.dump(data2save, open( "TreatedData_medical_test_train.pk", "wb" ) )
 
-    
-
-
+   
 """
 infile = open('./TreatedData_all5.pk','rb')
 SavedData = pickle.load(infile)
---
+++ Studying unknown bias in bios dataset/fit_NLP_model.py
@@ -0,0 +1,94 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
+import torchvision.models as models
+from collections import OrderedDict
+
+import time
+import pickle
+import pandas
+import numpy as np # to handle matrix and data operation
+import matplotlib.pyplot as plt   #image visualisation
+import scipy.stats as st
+
+
+
+def fit_NLP_model(model,X_train,Masks_train,y_train, f_loss_attach=nn.MSELoss() , EPOCHS = 5, BATCH_SIZE = 32,DEVICE='cpu',optim_lr=0.0001):
+    """
+    -> X_train: input pytorch tensor are supposed to have a shape structured as [NbObs,:], [NbObs,:,:], or [NbObs,:,:,:].
+    -> Masks_train: same size as X_train; masks the values of X_train to indicate whether a word is present or not in the text.
+    -> y_train: true output pytorch tensor. Supposed to be 2D (for one-hot encoding or others), or 1D (eg for binary classification)
+    -> f_loss_attach: Data attachment term in the loss. Can be eg  nn.MSELoss(), nn.BCELoss(), ... . Must be coherent with the model outputs and the true outputs.
+    """
+
+    outputdatadim=len(y_train.shape)-1  #dimension of the output data (-1 takes into account the fact that the first dimension corresponds to the observations)
+
+    optimizer = torch.optim.Adam(model.parameters(),lr=optim_lr) #,lr=0.001, betas=(0.9,0.999))
+
+    model.train()
+
+    n=X_train.shape[0]
+
+    Lists_Results={}
+    Lists_Results['Loss']=[]          #loss of the data attachement term
+
+
+    epoch=0
+    while epoch<EPOCHS:
+        obsIDs=np.arange(n)
+        np.random.shuffle(obsIDs)
+
+        batch_start=0
+        batchNb=0
+
+        while batch_start+BATCH_SIZE < n:
+
+            #1) mini-batch predictions
+
+            #1.1) get the observation IDs
+            Curr_obsIDs=obsIDs[batch_start:batch_start+BATCH_SIZE]
+
+            X_batch = X_train[Curr_obsIDs,:].long().to(DEVICE)
+            Masks_batch = Masks_train[Curr_obsIDs,:].to(DEVICE)
+
+            if outputdatadim==0:
+              y_batch = y_train[Curr_obsIDs].view(-1,1).float()  #as the outputs are in a 1d vector
+            elif outputdatadim==1:
+              y_batch = y_train[Curr_obsIDs,:].float()
+
+
+            #2.3) set the NN gradient to zero
+            optimizer.zero_grad()
+
+            #2.4) mini-batch prediction
+            output = model(ids=X_batch, mask=Masks_batch)
+
+            #3) compute the attachement term loss
+            loss=f_loss_attach(output, y_batch.to(DEVICE))
+
+            loss.backward()
+            optimizer.step()
+
+            #6) update the first observation of the batch
+            batch_start+=BATCH_SIZE
+            batchNb+=1
+
+            #7) save pertinent information to check the convergence
+            locLoss=loss.item()
+            Lists_Results['Loss'].append(locLoss)
+            print("epoch "+str(epoch)+" -- batchNb "+str(batchNb)+": Loss="+str(Lists_Results['Loss'][-1]))
+
+        #update the epoch number
+        epoch+=1
+
+
+    model_cpu=model.to('cpu')
+    saved_models = { "model": model_cpu }
+    pickle.dump( saved_models, open( 'saved_model.p', "wb" ) )
+    # -> saved_models = pickle.load( open( "saved_model.p", "rb" ) )
+    # -> model_cpu=saved_models["model"]
+    # -> model=model_cpu.to(DEVICE)
+
+    return Lists_Results
