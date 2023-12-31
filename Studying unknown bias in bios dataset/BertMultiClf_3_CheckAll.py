7a0deb07621b61825f9ec3d845059f576da2b53b
TomFiz <tom.fi@hotmail.fr>
2023-12-31 18:46:13 +0100

    pushing confusion matrixes

--- Studying unknown bias in bios dataset/BertMultiClf_3_CheckAll.py
+++ Studying unknown bias in bios dataset/BertMultiClf_3_CheckAll.py
@@ -194,6 +194,7 @@ Lst1=np.where(S_select>0.5)[0]
 confusionMatrix0=sklearn.metrics.confusion_matrix(y_true_labels[Lst0],y_pred_labels[Lst0],normalize='pred')
 confusionMatrix1=sklearn.metrics.confusion_matrix(y_true_labels[Lst1],y_pred_labels[Lst1],normalize='pred')
 
+
 Lst0_error=np.where(y_true_labels[Lst0]!=y_pred_labels[Lst0])[0]
 print('number of errors in group 0:',len(Lst0_error))
 Lst1_error=np.where(y_true_labels[Lst1]!=y_pred_labels[Lst1])[0]
@@ -211,6 +212,7 @@ data2save_1['predicted_job'] = y_pred_labels[Lst1_error]
 
 pickle.dump(data2save_0, open( "Treated_Error0.pkl", "wb" ) )
 pickle.dump(data2save_1, open( "Treated_Error1.pkl", "wb" ) )
+
 
 
 ax=plt.axes()
